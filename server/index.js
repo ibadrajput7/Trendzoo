f4c05cedc994f82c02c8449be6e4a37f0dd7f317
ibadrajput7 <ahmedibad0012@gmail.com>
2026-09-19 02:34:55 +0500

    Updated Index.js CORS section from File

--- server/index.js
+++ server/index.js
@@ -18,6 +18,8 @@ app.use(
     cors({
         origin: process.env.CLIENT_URL,
         credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
